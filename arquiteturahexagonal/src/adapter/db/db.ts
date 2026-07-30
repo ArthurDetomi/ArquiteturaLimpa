4f1e6d6018fd13ad07f32e3c92556831cac28839
Arthur Detomi <arthurdetomi@gmail.com>
2026-07-30 15:12:30 -0300

    Remove console.log

--- arquiteturahexagonal/src/adapter/db/db.ts
+++ arquiteturahexagonal/src/adapter/db/db.ts
@@ -4,14 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
 // Create the connection pool. The pool-specific settings are the defaults
 export const pool: mysql.Pool = mysql.createPool({
   host: process.env.DB_HOST,
